a5c7e6b5f1401ac8aa5af173cf3db8faa2ee11dd
Manida Vilay <vilaymanida@gmail.com>
2024-11-25 15:50:41 +0100

    install mongodb, mongoose and connect to db

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('This is from node API')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
 
-app.get('/', (req, res) => {
-    res.send('This is from node API')
+mongoose.connect("mongodb+srv://manidavilay:JitsazaQDIR3c23c@backenddb.7yoh2.mongodb.net/Node-Express-Course?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected to database !")
+}).catch(() => {
+    console.log("Connection failed")
 })
