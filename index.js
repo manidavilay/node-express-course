1d8dd41233394264ca0ededfb75445a6adb5a228
Manida Vilay <vilaymanida@gmail.com>
2024-11-25 17:54:19 +0100

    add error message for get product by id API

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ app.get("/api/product/:id", async (req, res) => {
     try {
         const { id } = req.params
         const product = await Product.findById(id)
+
+        // if product doesn't exist
+        if (!product) {
+            res.status(404).json({ message: "Product not found !" })
+        }
+        
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({ message: error.message })
