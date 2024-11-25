63dc515f834d73cfb55d2b7477fd379ecf0958b1
Manida Vilay <vilaymanida@gmail.com>
2024-11-25 17:11:01 +0100

    create GET all API

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get("/", (req, res) => {
     res.send("This is from node API")
 })
 
+// Get all products route
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 // Post product route
 app.post("/api/products", async (req, res) => {
     try {
